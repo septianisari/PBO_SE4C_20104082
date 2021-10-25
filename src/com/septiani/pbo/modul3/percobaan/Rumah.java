2d3949494062567806f5b483e37519b2d8381846
septianisari <septianisarimanik@gmail.com>
2021-10-25 09:57:39 +0700

    Src Percobaan Class Rumah

---
+++ src/com/septiani/pbo/modul3/percobaan/Rumah.java
@@ -0,0 +1,9 @@
+package com.septiani.pbo.modul3.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class Rumah {
+    String alamat_rumah;
+    public void setAlamat(String x) {
+        alamat_rumah=x;}
+}
