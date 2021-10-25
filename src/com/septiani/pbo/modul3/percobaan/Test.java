480afb06f01810f0a39dc9fe3b08a03853deaaf1
septianisari <septianisarimanik@gmail.com>
2021-10-25 09:58:10 +0700

    Src Percobaan Class Test

---
+++ src/com/septiani/pbo/modul3/percobaan/Test.java
@@ -0,0 +1,10 @@
+package com.septiani.pbo.modul3.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class Test {
+    public static void main(String args[]) {
+        Rumah r=new Rumah();
+        r.setAlamat("Alamat saya di Jalan Pramuka");
+        System.out.println(r.alamat_rumah);}
+}
