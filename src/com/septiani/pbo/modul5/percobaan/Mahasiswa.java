d851f82a517cd1dd1050f54a4f4838504574401f
septianisari <septianisarimanik@gmail.com>
2021-11-08 10:01:16 +0700

    Src Percobaan Class Mahasiswa

---
+++ src/com/septiani/pbo/modul5/percobaan/Mahasiswa.java
@@ -0,0 +1,13 @@
+package com.septiani.pbo.modul5.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class Mahasiswa {
+    private int nrp;
+    private String nama;
+    public Mahasiswa(int
+                             nrp, String nama) {
+        this.nrp=nrp;
+        this.nama=nama;
+    }
+}
