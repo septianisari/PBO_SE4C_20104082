9c0aba315002929aac853d4bb0c99b8af89a8426
septianisari <septianisarimanik@gmail.com>
2021-11-21 22:22:08 +0700

    Src Percobaan Class Electronics

---
+++ src/com/septiani/pbo/modul7/percobaan/Electronics.java
@@ -0,0 +1,36 @@
+package com.septiani.pbo.modul7.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class Electronics {
+    private String name;
+    private boolean isActivated = false;
+
+    public Electronics(){
+        this.name = "A Device";
+        System.out.println(this.getName() + " has been spawned!");
+    }
+
+    public Electronics(String name){
+        this.name = name;
+        System.out.println(this.getName() + " has been spawned!");
+    }
+    public String getName(){
+        return name;
+    }
+
+
+    public void activate(){
+        this.isActivated = true;
+        System.out.println(this.getName() + "has been actvated!");
+        this.oneActivate();
+    }
+
+    public void deactivate(){
+        this.isActivated = false;
+        System.out.println(this.getName() + " has been deactivated!");
+        this.oneDeactivate();
+    }
+    public void oneActivate(){}
+    public void oneDeactivate(){}
+}
