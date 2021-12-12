c015e708e0aba3f72f9dc460439b5ba0f774cb55
septianisari <septianisarimanik@gmail.com>
2021-12-12 22:21:54 +0700

    Src Class Employee

---
+++ src/com/septiani/pbo/modul8/percobaan/Employee.java
@@ -0,0 +1,35 @@
+package com.septiani.pbo.modul8.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class Employee {
+    private String name;
+    private String address;
+    private int number;
+
+    public Employee(String name, String address, int number){
+        this.name= name;
+        this.address= address;
+        this.number= number;
+    }
+    public void checkMail(){
+        System.out.println("Checking Mails for" +address);
+    }
+    public String toString(){
+        return name + "" + address +" "+ number;
+    }
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
