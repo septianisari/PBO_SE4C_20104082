a66d6b69e130879de5ee67ca647c50645a57c6fd
septianisari <septianisarimanik@gmail.com>
2021-12-12 22:23:44 +0700

    Src Class VMIDemo

---
+++ src/com/septiani/pbo/modul8/percobaan/VMIDemo.java
@@ -0,0 +1,13 @@
+package com.septiani.pbo.modul8.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class VMIDemo {
+    public static void main(String[] args) {
+        Salary s = new Salary("Julius", "Kubar", 3, 5000);
+        s.checkMail();
+
+        Employee e = new Salary("Ucok", "Jabar", 4, 2500);
+        e.checkMail();
+    }
+}
