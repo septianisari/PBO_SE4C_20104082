a0fd14af426e49dc629b87db225a2eaebadee296
septianisari <septianisarimanik@gmail.com>
2021-12-19 11:40:31 +0700

    Src Percobaan Class Percobaan2

---
+++ src/com/septiani/pbo/modul9/percobaan/Percobaan2.java
@@ -0,0 +1,10 @@
+package com.septiani.pbo.modul9.percobaan;
+
+// Septiani Sari (20104082) //
+
+public class Percobaan2 {
+    public static void main(String[] args) {
+        int bil = 10 ;
+        System.out.println(bil/0);
+    }
+}
